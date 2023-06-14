61a5118ba99568da33eb16f9291f7a4ad4e4dcff
Van <1027154225@qq.com>
2023-06-14 15:56:34 +0800

    style(custom): 优化代码格式

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,20 @@
 import useUserStore from './store/user';
 import { ElMessage } from 'element-plus';
 import { useRouter } from 'vue-router';
-const router = useRouter()
+const router = useRouter();
 
-const userStore = useUserStore()
+const userStore = useUserStore();
 if (userStore.userName) {
-    userStore.checkUser().then(res => {
+    userStore.checkUser().then((res) => {
         if (res.code !== 200) {
             ElMessage({
                 type: 'error',
-                message: '用户登录已过期'
-            })
-            router.push('/login')
+                message: '用户登录已过期',
+            });
+            router.push('/login');
         }
-    })
+    });
 }
-
 </script>
 
 <style lang="scss">
